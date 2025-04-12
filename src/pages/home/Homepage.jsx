50d91e575bd5240af67b3674198c89b3f349dc1e
Adir Noyman <ofahol2007@hotmail.com>
2025-04-12 14:10:47 +0300

    .

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -4,7 +4,21 @@ import './homepage.css'
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <Link to="/dashboard" className='logo'>Dashboard 🤓</Link>
+      <img src="/orbital.png" className='orbital' alt="orbital" />
+     <div className="leftSide">
+      <h1>ADIROS DEV</h1>
+      <h2>Supercharge your charge</h2>
+      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga voluptatem maxime reprehenderit.</h3>
+      <Link to="/" aria-label='get started'>Get Started</Link>
+     </div>
+     <div className="rightSide">
+      <div className="imgContainer">
+        <div className="bgContainer">
+          <div className="bg"></div>
+        </div>
+        <img src="/bot.png" alt="bot"  className='bot'/>
+      </div>
+     </div>
     </div>
   )
 }
